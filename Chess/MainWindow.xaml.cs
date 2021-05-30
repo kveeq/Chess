7ce0f070fe903175e972fdecfb3b383b481aa500
ильсаф гарифуллин <ilsgarifullin@yandex.ru>
2021-05-30 22:34:03 +0300

    add a interface

--- Chess/MainWindow.xaml.cs
+++ Chess/MainWindow.xaml.cs
@@ -12,17 +12,65 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ChessLibrary;
 
 namespace Chess
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+
+        private string name;
+        private int _column;
+        private int _row;
+        ChessFigures figures = null;
+        bool state = false;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            state = !state;
+            Button _btn = sender as Button;
+            _row = (int)_btn.GetValue(Grid.RowProperty) + 1;
+            _column = (int)_btn.GetValue(Grid.ColumnProperty) + 1;
+
+            if (state)
+            {
+                name = _btn.Name;
+                Hui(name, _column, _row);
+            }
+            else
+            {
+                MessageBox.Show($"{figures.Move(_column, _row)} \n row {_row} column {_column}");
+            }
+        }
+
+        private void Hui(string _name, int x, int y)
+        {
+            switch (_name)
+            {
+                case "King":
+                    figures = new King(x, y);
+                    //figures.Move();
+                    break;
+                case "Queen":
+                    figures = new Queen(x, y);
+                    break;
+                case "Bishop":
+                    figures = new Bishop(x, y);
+                    break;
+                case "Knight":
+                    figures = new Knight(x, y);
+                    break;
+                case "Rook":
+                    figures = new Rook(x, y);
+                    break;
+                case "Pawn":
+                    figures = new Pawn(x, y);
+                    break;
+            }
         }
     }
 }
--- ChessLibrary/Class1.cs
+++ ChessLibrary/Class1.cs
@@ -6,8 +6,136 @@ using System.Threading.Tasks;
 
 namespace ChessLibrary
 {
-    public class Class1
+    public class ChessFigures
     {
+        public int x1;
+        public int y1;
+
+        public ChessFigures(int newX, int newY)
+        {
+            x1 = newX;
+            y1 = newY;
+        }
+
+        public virtual bool Move(int newX, int newY)
+        {
+            return false;
+        }
+    }
+
+    public class King : ChessFigures
+    {
+        public King(int x1, int y1) : base(x1, y1)
+        {
+            Console.WriteLine("King Constructor");
+        }
+
+        public override bool Move(int newX, int newY)
+        {
+            if (Math.Abs(x1 - newX) <= 1 && Math.Abs(y1 - newY) <= 1)
+            {
+                x1 = newX;
+                y1 = newY;
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+
+    public class Queen : ChessFigures
+    {
+        public Queen(int x1, int y1) : base(x1, y1)
+        {
+            Console.WriteLine("Queen Constructor");
+        }
+
+        public override bool Move(int newX, int newY)
+        {
+            if (x1 == newX || y1 == newY ||
+                     Math.Abs(x1 - newX) == Math.Abs(y1 - newY))
+            {
+                x1 = newX;
+                y1 = newY;
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+
+    public class Bishop : ChessFigures
+    {
+        public Bishop(int x1, int y1) : base(x1, y1)
+        {
+            Console.WriteLine("Bishop Constructor");
+        }
+
+        public override bool Move(int newX, int newY)
+        {
+            if (Math.Abs(x1 - newX) == Math.Abs(y1 - newY))
+            {
+                x1 = newX;
+                y1 = newY;
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+
+    public class Knight : ChessFigures
+    {
+        public Knight(int x1, int y1) : base(x1, y1)
+        {
+            Console.WriteLine("Knight Constructor");
+        }
+
+        public override bool Move(int newX, int newY)
+        {
+            if ((Math.Abs(x1 - newX) == 2 && Math.Abs(y1 - newY) == 1) ||
+                    (Math.Abs(x1 - newX) == 1 && Math.Abs(y1 - newY) == 2))
+            {
+                x1 = newX;
+                y1 = newY;
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+
+    public class Rook : ChessFigures
+    {
+        public Rook(int x1, int y1) : base(x1, y1)
+        {
+            Console.WriteLine("Rook Constructor");
+        }
+
+        public override bool Move(int newX, int newY)
+        {
+            if (x1 == newX || y1 == newY)
+            {
+                x1 = newX;
+                y1 = newY;
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+
+
+    public class Pawn : ChessFigures
+    {
+        public Pawn(int newX, int newY) : base(newX, newY)
+        { }
+
+        public override bool Move(int newX, int newY)
+        {
+            return ((x1 == newX && y1 == 2 && y1 + 2 >= newY) ||
+                    (x1 == newX && y1 + 1 == newY));
+        }
 
     }
 }
